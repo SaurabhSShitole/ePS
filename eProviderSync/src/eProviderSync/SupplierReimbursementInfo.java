692906a116e7338c309f4cf8fbb681df00bbdb83
saurabh.shitole <saurabh.shitole@TMGLAP846.TMG.COM>
2022-06-23 14:20:32 +0530

    1

--- eProviderSync/src/eProviderSync/SupplierReimbursementInfo.java
+++ eProviderSync/src/eProviderSync/SupplierReimbursementInfo.java
@@ -28,86 +28,102 @@ public class SupplierReimbursementInfo {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60000));
 		driver.get("https://bcbsne-epscfg.simplifyhealthcare.com/ConsumerAccount/Foundationsearch");
-        driver.findElement(By.id("UserName")).sendKeys("saurabh.shitole");
-        driver.findElement(By.id("Password")).sendKeys("Summer@2018#");
+		driver.findElement(By.id("UserName")).sendKeys("saurabh.shitole");
+		driver.findElement(By.id("Password")).sendKeys("Summer@2018#");
 
-        driver.findElement(By.xpath("//*[contains(@type,'submit')]")).click();
+		driver.findElement(By.xpath("//*[contains(@type,'submit')]")).click();
 
-        // Goto Portfolio
-        driver.findElement(By.xpath("//a[@data-original-title='Portfolio Provider Management']")).click();
-        
-        //open existing supplier
-        
-    	WebElement add = driver.findElement(By.xpath("//td[@id=\"btnPortfolioSearchNew\"]"));
-		JavascriptExecutor E=(JavascriptExecutor)driver;
+		// Goto Portfolio
+		driver.findElement(By.xpath("//a[@data-original-title='Portfolio Provider Management']")).click();
+
+		// open existing supplier
+
+		WebElement add = driver.findElement(By.xpath("//td[@id=\"btnPortfolioSearchNew\"]"));
+		JavascriptExecutor E = (JavascriptExecutor) driver;
 		E.executeScript("arguments[0].click()", add);
-		
+
 		WebElement DD = driver.findElement(By.xpath("//select[@id=\"providerTypeDDL\"]"));
-		Select D=new Select(DD);
-		
-		WebDriverWait w=new WebDriverWait(driver, Duration.ofSeconds(60000));
+		Select D = new Select(DD);
+
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(60000));
 		w.until(ExpectedConditions.elementToBeClickable(DD));
 		D.selectByVisibleText("Supplier");
-		driver.findElement(By.xpath("//input[@id=\"TaxID\"]")).sendKeys("273899944",Keys.ENTER);
+		driver.findElement(By.xpath("//input[@id=\"TaxID\"]")).sendKeys("987879878", Keys.ENTER);
 		driver.findElement(By.xpath("//button[@id=\"btnTaxIDSearch\"]")).click();
-		driver.findElement(By.xpath("//button[@id=\"btnOpenProvider\"]")).click();		
-      
-        
-        //To select section
-        Select sections=new Select(driver.findElement(By.xpath("//div[@id=\"sectionMenu\"]/div/select")));
-        sections.selectByVisibleText("Supplier Reimbursement Information");
-           
-        Select networkLevel=new Select(driver.findElement(By.xpath("//select[@data-link=\"SupplierReimbursementInformation.IsReimbursementInformationapplicableataSupplierNetworkLevel\"]")));
-        networkLevel.selectByValue("Yes");
-       
-        //To select Which reimbursement structures are applicable
-       WebElement reimbursmentStructure = driver.findElement(By.xpath("//select[@data-link=\"SupplierReimbursementInformation.WhichreimbursementstructuresareapplicableataSupplierNetworkLevel\"]"));
-       Select s=new Select(reimbursmentStructure);
-       if (s.isMultiple()) 
-       {
-    	   s.selectByVisibleText("Professional");
-    	   s.selectByVisibleText("ASC");
-    	   s.selectByVisibleText("Institutional");
-	   }
-       
-       JavascriptExecutor executor = (JavascriptExecutor) driver;
-       executor.executeScript("window.scrollBy(0,250)");
+		driver.findElement(By.xpath("//button[@id=\"btnOpenProvider\"]")).click();
 
-       WebElement r = driver.findElement(By.xpath("//input[@value=\"Reimbursement Table Information - Supplier\"]"));
-       executor.executeScript("arguments[0].click()", r);
-       
-	  
-       try {
-    	   WebElement manualOverride = driver.findElement(By.xpath("//span[@title=\"false\"]"));  
-    	   Actions mo=new Actions(driver);
-           mo.doubleClick(manualOverride).build().perform();
-      
+		// To select section
+		Select sections = new Select(driver.findElement(By.xpath("//div[@id=\"sectionMenu\"]/div/select")));
+		sections.selectByVisibleText("Supplier Reimbursement Information");
+
+		Select networkLevel = new Select(driver.findElement(By.xpath(
+				"//select[@data-link=\"SupplierReimbursementInformation.IsReimbursementInformationapplicableataSupplierNetworkLevel\"]")));
+		networkLevel.selectByValue("Yes");
+
+		// To select Which reimbursement structures are applicable
+		WebElement reimbursmentStructure = driver.findElement(By.xpath(
+				"//select[@data-link=\"SupplierReimbursementInformation.WhichreimbursementstructuresareapplicableataSupplierNetworkLevel\"]"));
+		Select s = new Select(reimbursmentStructure);
+		if (s.isMultiple()) {
+			s.selectByVisibleText("Professional");
+			s.selectByVisibleText("ASC");
+			s.selectByVisibleText("Institutional");
+		}
+
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("window.scrollBy(0,250)");
+
+		WebElement r = driver.findElement(By.xpath("//input[@value=\"Reimbursement Table Information - Supplier\"]"));
+		executor.executeScript("arguments[0].click()", r);
 		
-	} catch (Exception e) {
-		  WebElement manualOverride1 = driver.findElement(By.xpath("//div[@comp-id=\"204\"]"));  
-	       Actions mo=new Actions(driver);
-	       mo.doubleClick(manualOverride1).build().perform();
+		
+		
+	    try {
+		WebElement manualOverride = driver.findElement(By.xpath("//span[@title=\"false\"]"));
+		Actions mo = new Actions(driver);
+		mo.doubleClick(manualOverride).build().perform();
 
-	}
-       driver.findElement(By.xpath("//input[@value=\"Category Rates - Supplier Network\"]")).click();
-       driver.findElement(By.xpath("//div[@style=\"width: 163px; left: 670px; white-space: normal;\"]")).click();
+	     } 
+	    catch (Exception e) {
+		WebElement manualOverride1 = driver.findElement(By.xpath("//div[@col-id=\"ManualOverride\" and @ style=\"width: 171px; left: 499px; text-align: center;\"]"));
+		Actions mo = new Actions(driver);
+		mo.doubleClick(manualOverride1).build().perform();
 
-       //To select table name 
-       driver.findElement(By.xpath("//div[@style=\"width: 163px; left: 670px; white-space: normal;\"]")).click();
-       Select tablename=new Select(driver.findElement(By.xpath("//select[@id=\"idTableName_0\"]")));
-       tablename.selectByVisibleText("Home Health OP 2019");
-      
-       //Scroll
-       executor.executeScript("window.scrollBy(0,500)");
+	     }
+		
+		
+		driver.findElement(By.xpath("//div[@style=\"width: 163px; left: 670px; white-space: normal;\"]")).click();
+		driver.findElement(By.xpath("//input[@value=\"Category Rates - Supplier Network\"]")).click();
 
-       driver.findElement(By.xpath("//input[@value=\"Category Rates - Supplier\"]")).click();
-       driver.findElement(By.xpath("//input[@value=\"Anesthesia Conversion Factor - Supplier Network\"]")).click();
-      
-      // driver.findElement(By.xpath("value=\"Anesthesia Conversion Factor - Supplier\"")).click();
-     
-      
-       
-         
+
+		// To select table name
+		driver.findElement(By.xpath("//div[@style=\"width: 163px; left: 670px; white-space: normal;\"]")).click();
+		Select tablename = new Select(driver.findElement(By.xpath("//select[@id=\"idTableName_0\"]")));
+		tablename.selectByVisibleText("ASC 2009");
+		Thread.sleep(5000);
+
+		// Scroll
+		//executor.executeScript("window.scrollBy(0,300)");
+		executor.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+
+
+		driver.findElement(By.xpath("//input[@value=\"Category Rates - Supplier\"]")).click();
+		driver.findElement(By.xpath("//input[@value=\"Anesthesia Conversion Factor - Supplier Network\"]")).click();
+		executor.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+		
+		
+		WebElement categoryratesMO = driver.findElement(By.xpath("(//div[@col-id=\"ManualOverride\"])[4]/span"));
+		Actions action=new Actions(driver);
+		action.doubleClick(categoryratesMO).build().perform();
+		//driver.findElement(By.xpath("(//div[@col-id=\"ServiceCategoryReimPercent\"])[2]")).sendKeys("014",Keys.ENTER);
+		
+		
+		//driver.findElement(By.xpath("//input[@value=\"Anesthesia Conversion Factor - Supplier\"]")).click();
+		
+		
+		driver.findElement(By.xpath("//input[@value=\"Benefit Network Discount - Supplier Network\"]")).click();
+		//executor.executeScript("window.scrollBy(0,100)");
+
 
 	}
 
